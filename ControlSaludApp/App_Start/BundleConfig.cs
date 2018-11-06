c6786d60ae2d7ec4027baaee8e0f5beb0aaf7b8c
Emerson <emersontenoriorojas@hotmail.com>
2018-11-06 01:12:11 -0500

    MintyDesingForm

---
+++ ControlSaludApp/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ControlSaludApp
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+
+            bundles.Add(new StyleBundle("~/bundles/css")
+            .Include(
+                    "~/Content/css/bootstrap.css",
+                    "~/Content/css/bootstrap.min.css",
+                    "~/Content/css/custom.min.css"
+                    ));
+
+            bundles.Add(new ScriptBundle("~/bundles/js")
+            .Include(
+            "~/Content/js/bootstrap.min.js",
+            "~/Content/js/custom.js",
+            "~/Content/js/jquery.min.js",
+            "~/Content/js/popper.min.js"
+            ));
+
+
+        }
+    }
+}
